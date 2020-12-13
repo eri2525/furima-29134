084ba977062aaef58290e5dba7084d5591c38afe
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-14 01:37:39 +0900

    Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, :birthday, presence: true
   # validates :email,  uniqueness: true, inclusion: {in:%w(@)}
-  validates :first_name, :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_furigana, :last_furigana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password,  length: { minimum: 6 }, format:{with: VALID_PASSWORD_REGEX }, confirmation: true
+  validates :first_name, :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_furigana, :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
 end
