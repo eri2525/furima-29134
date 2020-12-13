a9706081e9e0f0f87fa64ad3c87c7e16791ebda0
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-14 01:29:42 +0900

    テストコード作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {"ああああ"}
+    last_name             {"いいいい"}
+    first_furigana        {"アアアア"}
+    last_furigana         {"イイイイ"}
+    birthday              {Faker::Date.in_date_period} 
+  end
+end
