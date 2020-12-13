7b2e70942c69dfeee53a9bbcb70ce9207210c419
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-13 20:44:01 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :birthday, presence: true
+  # validates :email,  uniqueness: true, inclusion: {in:%w(@)}
+  validates :first_name, :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_furigana, :last_furigana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password,  length: { minimum: 6 }, format:{with: VALID_PASSWORD_REGEX }, confirmation: true
 end
