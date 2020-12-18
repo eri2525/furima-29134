4322c476c3851fe709b1835495e650cb727432de
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-18 21:52:40 +0900

    active_hashを導入する

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :delivery_fee, :prefecture, :delivery_time
+
+  validates :name, :description, :category_id, :conditon_id, :delivery_fee_id, :prefecture_id, :delivery_fee_id, :price, presence: true
+
+  validates :category_id, :conditon_id, :delivery_fee_id, :prefecture_id, :delivery_fee_id, numericality: { other_than: 1 }
 end
--- db/migrate/20201218085524_create_items.rb
+++ db/migrate/20201218085524_create_items.rb
@@ -1,14 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :name
-      t.string  :description
-      t.integer  :category_id
-      t.integer  :condition_id
-      t.integer  :delivery_fee_id
-      t.integer  :prefecture_id
-      t.integer  :delivery_time_id
-      t.integer  :price
+      t.string  :name                       , null: false
+      t.string  :description                , null: false
+      t.integer  :category_id               , null: false
+      t.integer  :condition_id              , null: false
+      t.integer  :delivery_fee_id           , null: false
+      t.integer  :prefecture_id             , null: false
+      t.integer  :delivery_time_id          , null: false
+      t.integer  :price                     , null: false
       t.references :user, foreign_key: true
       t.timestamps
     end
