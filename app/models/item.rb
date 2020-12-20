c22dd264a574e5a1370c0d856663f4ff755f636d
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-20 21:24:51 +0900

    モデルの単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,9 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, :price, presence: true
-
-  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, numericality: { other_than: 1 }
+  validates :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, presence: true, numericality: { other_than: 1 }
+  validates :name, presence: true, length: {maximum: 40}
+  validates :description, presence: true, length: {maximum: 1000}
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/}, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :image, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context "商品の出品ができる場合" do
+      it "nameとimageとdescriptionとcategory_idとcondition_idとdelivery_fee_idとdelivery_time_idとpriceがあれば商品の出品ができる" do
+        expect(@item).to be_valid
+      end
+      it "priceの範囲が300~9999999の間であれば商品の出品ができる" do
+        @item.price = 400
+        expect(@item).to be_valid
+      end
+      it "priceが半角英数字であれば商品の出品ができる" do
+        @item.price = 400
+        expect(@item).to be_valid
+      end
+    end
+    context "商品の出品ができない場合" do
+      it "nameがなければ商品の出品ができない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end     
+      it "imageがなければ商品の出品ができない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end    
+      it "descriptionがなければ商品の出品ができない" do
+        @item.description = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end    
+      it "category_idがなければ商品の出品ができない" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end   
+      it "condition_idがなければ商品の出品ができない" do
+        @item.condition_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end   
+      it "delivery_fee_idがなければ商品の出品ができない" do
+        @item.delivery_fee_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end   
+      it "delivery_time_idがなければ商品の出品ができない" do
+        @item.delivery_time_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end   
+      it "priceの範囲が300以上でなければ商品の出品ができない" do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end   
+      it "priceの範囲が9,999,999以下でなければ商品の出品ができない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end   
+      it "priceが半角英数字でなければ商品の出品ができない" do
+        @item.price = "aaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it "ユーザーが紐付いていないとツイートは保存できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
