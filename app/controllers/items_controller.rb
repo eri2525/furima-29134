24dcf070ac63b3f2ef5226309c5e3de0e688ad76
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-21 16:38:14 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,5 +21,6 @@ end
 private
 
 def item_params
-  params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, :price, :image).merge(user_id: current_user.id)
+  params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
+                               :delivery_time_id, :price, :image).merge(user_id: current_user.id)
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :item do
-    name {Faker::Lorem.sentence}
-    description {Faker::Lorem.sentence}
-    category_id {Faker::Number.within(range: 2..11)}
-    condition_id {Faker::Number.within(range: 2..7)}
-    delivery_fee_id {Faker::Number.within(range: 2..3)}
-    delivery_time_id {Faker::Number.within(range: 2..4)}
-    prefecture_id {Faker::Number.within(range: 2..48)}
-    price {Faker::Number.positive}
-    association :user 
+    name { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentence }
+    category_id { Faker::Number.within(range: 2..11) }
+    condition_id { Faker::Number.within(range: 2..7) }
+    delivery_fee_id { Faker::Number.within(range: 2..3) }
+    delivery_time_id { Faker::Number.within(range: 2..4) }
+    prefecture_id { Faker::Number.within(range: 2..48) }
+    price { Faker::Number.positive }
+    association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
