6273d53efe98393dd00495300b5c4ea1000130bf
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-31 20:07:20 +0900

    updateアクションを定義する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+    redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   def create
     @item = Item.new(item_params)
     if @item.save
