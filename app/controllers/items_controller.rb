cb748f0c140035b685f553cad7475e992955cf15
eri2525 <71331326+eri2525@users.noreply.github.com>
2020-12-31 20:13:03 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-    redirect_to action: :show
+      redirect_to action: :show
     else
       render :edit
     end
